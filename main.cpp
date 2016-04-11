ec40a68bb2bc5edfe9ad24d414cdc86f88ac2b95
meongseo <MeongHee@SeoMeongs-MacBook-Pro.local>
2016-04-10 22:08:06 -0400

    Header included

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/calib3d/calib3d.hpp"
+#include "bow.h"
 #include <iostream>
 #include <string>
 #include <iomanip>
