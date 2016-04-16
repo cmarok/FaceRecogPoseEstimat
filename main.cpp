b1da87868177ffaa11eaf80df99619129f4c8104
meongseo <MeongHee@SeoMeongs-MacBook-Pro.local>
2016-04-15 23:55:28 -0400

    last update

--- main.cpp
+++ main.cpp
@@ -35,6 +35,9 @@ void partitionImages(vector<Mat> &faceSet, vector<Mat> &training, vector<Mat> &t
 void LoadQMUL(vector<vector<vector<Mat>>> &Faces);
 void LoadQMUL_cas(vector<vector<vector<Mat>>> &Faces_cas);
 void Display_subject(vector<vector<vector<Mat>>> &Faces, int Subject_number);
+double computePixelLBP(const Mat input);
+vector<vector<vector<Mat>>> PosesLBP(vector<vector<vector<vector<Mat>>>> &Pose);
+Mat LBP_pose_est(vector<vector<vector<Mat>>> Histograms, HeadPose hp, int level);
 
 int main(int argc, const char * argv[]) 
 {    
@@ -274,3 +277,224 @@ void Display_subject(vector<vector<vector<Mat>>> &Faces, int Subject_number)
     namedWindow( "hello", WINDOW_AUTOSIZE );
     waitKey(0);
 }
+
+//converts faces to LBP images
+vector<vector<vector<Mat>>> FacesLBP(vector<vector<vector<Mat>>> &Faces){
+    vector<vector<vector<Mat>>> LBP_faces = Faces;
+
+    
+    //for all of the images in the object
+    for (int k=0; k<Faces.size();k++){
+        for(int l=0;l<Faces[k].size();l++){
+            for(int m=0; m<Faces[k][l].size();m++){
+                
+                Mat image1 =Faces[k][l][m];
+                Mat image_out(image1.size(), CV_64F);
+    
+                
+                //for all of the pixels (not including border pixels)
+                for (int i = 1; i < image1.cols-1; i++){
+                    for (int j = 1; j < image1.rows-1; j++){
+                        //extract surrounding 3x3 matrix for every pixel
+                        Mat pixelNeighbors = image1(Rect(i - 1, j - 1, 3, 3));
+                        
+                        //comupte lbp
+                        image_out.at<double>(j, i) = computePixelLBP(pixelNeighbors);
+                    }
+                }
+                image_out.convertTo(image_out, CV_8U);
+                LBP_faces[k][l][m] = image_out;
+            }
+        }
+    }
+
+    return LBP_faces;
+}
+
+double computePixelLBP(const Mat input){
+    //compute LBP value for pixel
+    Mat pixel;
+    
+    input.convertTo(pixel, CV_32S);
+
+    
+    int center_cell = pixel.at<int>(1, 1);
+    vector<int> Binary_vec;
+    
+    //check every pixel and create 8bit array, starting at left top corner of matrix
+    Binary_vec.push_back(!(center_cell < pixel.at<int>(0, 0)));
+    Binary_vec.push_back(!(center_cell < pixel.at<int>(0, 1)));
+    Binary_vec.push_back(!(center_cell < pixel.at<int>(0, 2)));
+    Binary_vec.push_back(!(center_cell < pixel.at<int>(1, 2)));
+    Binary_vec.push_back(!(center_cell < pixel.at<int>(2, 2)));
+    Binary_vec.push_back(!(center_cell < pixel.at<int>(2, 1)));
+    Binary_vec.push_back(!(center_cell < pixel.at<int>(2, 0)));
+    Binary_vec.push_back(!(center_cell < pixel.at<int>(1, 0)));
+    
+    //make sure less than 2 transitions
+    int transitions = 0;
+    for (int i = 0; i < Binary_vec.size()-1; i++){
+        if (Binary_vec[i + 1] - Binary_vec[i] != 0)
+            transitions = transitions + 1;
+    }
+    if (Binary_vec[0] - Binary_vec[Binary_vec.size()-1] != 0){
+        transitions = transitions + 1;
+    }
+    //get LBP value
+    double LVPvalue = 0;
+    //if transitions are 2 or less, compute the LBP value, otherwise LVPvalue remains 0
+    if (transitions <= 2){
+        for (int i = 0; i < Binary_vec.size(); i++){
+            if (Binary_vec[i] == 1){
+                LVPvalue = LVPvalue + pow(2, (double)i);
+            }
+        }
+    }
+    //cout << LVPvalue << "\n";
+    //return LVP value
+    return LVPvalue;
+    
+}
+
+
+//divides LBP images into boxes and finds histograms for the boxes, concatantes them for each image
+//level=0 -> whole image (1x1)
+//level=1 -> 4 boxes  (2x2)
+//level=2 -> 16 boxes (4x4) etc
+vector<vector<vector<Mat>>> LBPHistograms(vector<vector<vector<Mat>>> &LBP_face, int level){
+
+    vector<vector<vector<Mat>>> ALL_Histograms = LBP_face;
+    
+    //goes through all the images in the datase/////////////////
+    for (int k=0; k<LBP_face.size();k++){
+        for(int l=0;l<LBP_face[k].size();l++){
+            for(int m=0; m<LBP_face[k][l].size();m++){
+                int divisions = pow(2,level);//number boxes per axis
+                int width = LBP_face[k][l][m].cols;
+                width = width/divisions; //width of each box
+                int height = LBP_face[k][l][m].rows;
+                height = height/divisions; //height of each box
+                Mat Image_Histogram;
+                
+                //goes through each of the boxes
+                for(int i=0; i<divisions; i++){
+                    for(int j=0;j<divisions; j++){
+                        
+                        //get box
+                        Mat patch = LBP_face[k][l][m];
+                        patch = patch(Rect(i*width, j*height, width-1, height-1));
+                        
+                        //Histogram settings
+                        Mat hist;
+                        int bins = 59;
+                        int histSize[] = {bins};
+                        float lranges[] = {0, 256};
+                        const float* ranges[] = {lranges};
+                        
+                        //get histogram of current box
+                        calcHist(&patch, 1, 0, cv::Mat(), hist, 1, histSize, ranges);
+                        
+                        //concatanate it to the rest of the boxes in the image
+                        if (i==0 && j==0){
+                            Image_Histogram=hist;
+                        }else{
+                            hconcat(Image_Histogram,hist,Image_Histogram);
+                        }
+                    }
+                }
+                ALL_Histograms[k][l][m]= Image_Histogram;
+            }
+        }
+    }
+    return ALL_Histograms;
+}
+
+
+Mat LBP_pose_est(vector<vector<vector<Mat>>> Faces_hist, HeadPose hp,int level){
+    Mat confusion = Mat::zeros(21,21,CV_64F);
+    vector<vector<vector<vector<Mat>>>> Pose_hist;
+    vector<string> tilt = {"-30", "-15", "+0", "+15", "+30"};
+    vector<string> pan = {"-90", "-75", "-60", "-45", "-30", "-15", "+0", "+15", "+30", "+45", "+60", "+75", "+90"};
+    
+    vector<string> Pan_code = {"000","010","020","030","040","050","060","070","080","090","100","110","120","130","140", "150","160","170","180"};
+    //only testing for 21 images
+    for(int i=0;i<hp.images.size();i++){
+        for(int j=0;j<hp.images[i].size();j++){
+            for(int k=0;k<hp.images[i][j].size();k++){
+                for (int l=0; l<hp.images[i][j][k].size(); l++) {
+                    
+                    if (l%2==0){//only if one of the 7 pan angles we are testing
+                        int divisions = pow(2,level);//number boxes per axis
+                        int width = hp.images[i][j][k][l].cols;
+                        width = width/divisions; //width of each box
+                        int height = hp.images[i][j][k][l].rows;
+                        height = height/divisions; //height of each box
+                        Mat Image_Histogram;
+                        
+                        //goes through each of the boxes
+                        for(int p=0; p<divisions; p++){
+                            for(int q=0;q<divisions; q++){
+                                
+                                //get box
+                                Mat patch = hp.images[i][j][k][l];
+                                patch = patch(Rect(p*width, q*height, width-1, height-1));
+                                
+                                //Histogram settings
+                                Mat hist;
+                                int bins = 59;
+                                int histSize[] = {bins};
+                                float lranges[] = {0, 256};
+                                const float* ranges[] = {lranges};
+                                
+                                //get histogram of current box
+                                calcHist(&patch, 1, 0, cv::Mat(), hist, 1, histSize, ranges);
+                                
+                                //concatanate it to the rest of the boxes in the image
+                                if (p==0 && q==0){
+                                    Image_Histogram=hist;
+                                }else{
+                                    hconcat(Image_Histogram,hist,Image_Histogram);
+                                }
+                            }
+                        }
+                        double dist, shortest_dist;
+                        int best_match_tilt,best_match_pan;
+                        
+                        for(int m=0;m<5;m++){ //tilt angles
+                            for (int n=0; n<19; n=n+3) {
+                                for(int o=0;o<Faces_hist.size();o++){
+                                    dist = compareHist(Faces_hist[o][m][n], Image_Histogram, CV_COMP_CHISQR);
+                                    if(m==0 && n==0 && o==0){
+                                        shortest_dist=dist;
+                                        best_match_tilt = m;
+                                        best_match_pan = n;
+                                        
+                                    }else if(shortest_dist>dist) {
+                                        shortest_dist=dist;
+                                        best_match_tilt = m;
+                                        best_match_pan = n;
+                                    
+                                    
+                                    }
+                                }
+                            }
+                        
+                        }
+                    
+                        if(best_match_tilt<2){
+                            confusion.at<double>(l/2, best_match_pan)++;
+                        }else if (best_match_tilt==2){
+                            confusion.at<double>(l/2 + 7, best_match_pan+7)++;
+                        }else{
+                            confusion.at<double>(l/2 + 14, best_match_pan+14)++;
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+    cout<<confusion<<endl;
+    return confusion;
+}
+
